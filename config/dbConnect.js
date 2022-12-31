3984b259c5549148082cdd02354577eba08d5c66
飯塚翔 <smac@smac.local>
2022-12-31 13:34:30 +0900

    errorFix2

--- config/dbConnect.js
+++ config/dbConnect.js
@@ -5,7 +5,7 @@ const dbConnect = () =>{
   if(mongoose.connection.readyState >= 1){//マングースの接続ができている
     return
   }
-  mongoose.connect(process.env.DB_LOCAL_URI,{//ローカルモンゴdb
+  mongoose.connect(process.env.DB_URI,{//ローカルモンゴdb
   // mongoose.connect(process.env.DB_URI,{//本番モンゴDB
     // useNewUrlParser:true,今はもう必要ないらしい
     // useUnifiedTopology: true,
